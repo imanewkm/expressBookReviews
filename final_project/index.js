47f0cbf53dca6468d0c587337597f7147502581a
imanewkm <imanewkm02@gmail.com>
2024-09-26 16:39:07 +0100

    .\final_project\

--- final_project/index.js
+++ final_project/index.js
@@ -6,10 +6,13 @@ const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
+// Middleware to parse JSON data
 app.use(express.json());
 
+// Configure session middleware
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
+// Authentication middleware for customer routes
 app.use("/customer/auth/*", function auth(req,res,next){
     // Check if user is logged in and has valid access token
     if (req.session.authorization) {
@@ -18,7 +21,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
         // Verify JWT token
         jwt.verify(token, "access", (err, user) => {
             if (!err) {
-                req.user = user;
+                req.user = user; // Store user information in the request
                 next(); // Proceed to the next middleware
             } else {
                 return res.status(403).json({ message: "User not authenticated" });
@@ -28,10 +31,15 @@ app.use("/customer/auth/*", function auth(req,res,next){
         return res.status(403).json({ message: "User not logged in" });
     }
 });
- 
+
+// Use general routes for public API endpoints
 const PORT =5000;
 
+// User customer routes for authenticated user actions
 app.use("/customer", customer_routes);
+
+// User general routes for public API endpoints
 app.use("/", genl_routes);
 
+// Start the server
 app.listen(PORT,()=>console.log("Server is running"));
